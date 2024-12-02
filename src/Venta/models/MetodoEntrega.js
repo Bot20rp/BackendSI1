255d03ee20edc86500353204f0d1e52052d1175a
MiguelCs03 <mangelcs2003@gmail.com>
2024-12-02 19:52:57 -0400

    para carrito

---
+++ src/Venta/models/MetodoEntrega.js
@@ -0,0 +1,19 @@
+import { DataTypes } from "sequelize";
+import { db } from "../../config/dbConfig.js";
+
+const MetodoEntrega = db.define('MetodoEntrega', {
+    M_entregaID: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    Nombre: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+    }
+}, {
+    tableName: 'MetodoEntrega',
+    timestamps: false
+});
+
+export default MetodoEntrega;
---
+++ src/Venta/models/Pedidos.js
@@ -0,0 +1,42 @@
+import { DataTypes } from "sequelize";
+import { db } from "../../config/dbConfig.js";
+import MetodoEntrega from "./MetodoEntrega.js";
+import NotaVenta from "./NotaVenta.js";
+
+const Pedidos = db.define('Pedidos', {
+    PedidoID: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    Estado: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+    },
+    M_entregaID: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: MetodoEntrega,
+            key: 'M_entregaID'
+        }
+    },
+    NotaVentaID: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: NotaVenta,
+            key: 'NotaVentaID'
+        }
+    }
+}, {
+    tableName: 'Pedidos',
+    timestamps: false
+});
+
+// Relaciones
+MetodoEntrega.hasMany(Pedidos, { foreignKey: 'M_entregaID' });
+Pedidos.belongsTo(MetodoEntrega, { foreignKey: 'M_entregaID' });
+
+NotaVenta.hasMany(Pedidos, { foreignKey: 'NotaVentaID' });
+Pedidos.belongsTo(NotaVenta, { foreignKey: 'NotaVentaID' });
+
+export default Pedidos;
