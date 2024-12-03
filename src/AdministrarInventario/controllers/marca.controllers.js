3672e6c0661d7bcdd927372277c0861e35fdacf8
BoteritoROOT <d.sajor2003rp@gmail.com>
2024-12-03 09:46:55 -0400

    prueba

--- src/AdministrarInventario/controllers/marca.controllers.js
+++ src/AdministrarInventario/controllers/marca.controllers.js
@@ -3,9 +3,9 @@ import { createBitacora } from '../../AdministrarUsuario/controllers/bitacora.co
 export const createMarca=async (req,res)=>{
     try {
         console.log(req.body)
-        const {Nombre ,Region}=req.body.data
-        await Marca.create({Nombre,Region})
-        const message = `Marca creada: ${Nombre} en ${Region}`;
+        const {Nombre ,Region,Descripcion}=req.body.data
+        await Marca.create({Nombre,Region,Descripcion})
+        const message = `Marca creada: ${Nombre} en ${Region} y ${Descripcion}`;
         await createBitacora({ UsuarioID: req.user.id, message }, res);
         res.status(200).json({msg:"marca creada"})
     } catch (error) {
--- src/AdministrarInventario/models/Marca.js
+++ src/AdministrarInventario/models/Marca.js
@@ -1,23 +1,34 @@
 import { db } from "../../config/dbConfig.js";
 import { DataTypes } from "sequelize";
 
-const marca =db.define('Marca', {
+const marca = db.define(
+  "Marca",
+  {
     MarcaID: {
       type: DataTypes.INTEGER,
       primaryKey: true,
-      autoIncrement: true
+      autoIncrement: true,
     },
     Nombre: {
       type: DataTypes.STRING(30),
-      allowNull: false
+      allowNull: false,
     },
     Region: {
       type: DataTypes.STRING(50),
-      allowNull: false
-    }
-  }, {
-    tableName: 'Marca',
-    timestamps: false 
-  });
-  export default marca;
+      allowNull: false,
+    },
+    Descripcion: { // Nueva columna
+      type: DataTypes.STRING(100), // Tamaño de 100 caracteres
+      allowNull: true, // Permitir valores nulos si no es obligatorio
+      defaultValue: "marcas nuevas", // Valor por defecto
+    },
+  },
+  {
+    tableName: "Marca",
+    timestamps: false,
+  }
+);
+
+export default marca;
+
   
